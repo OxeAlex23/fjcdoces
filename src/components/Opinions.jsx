c70af8d317fdc2527349d0e1b2c713cfd0e70f71
OxeAlex23 <souzaalexciano@gmail.com>
2025-10-05 11:13:31 -0300

    Update Opinions.jsx

--- src/components/Opinions.jsx
+++ src/components/Opinions.jsx
@@ -3,18 +3,19 @@ import './styles.css';
 
 
 function Opinions() {
-    const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
-    const [names, setNames] = useState([])
+    const [names, setNames] = useState([]);
+    const [opnions, setOpnions] = useState([]);
 
     useEffect(() => {
         fetch("https://apifjcdoces.onrender.com/opnion")
             .then((res) => res.json())
             .then((json) => {
                 const allNames = json.map(it => it.name);
+                const allOpnions = json.map(op => op.description);
                 setNames(allNames);
+                setOpnions(allOpnions);
                 setLoading(false);
-                console.log(allNames)
             })
             .catch((err) => {
                 console.error(err);
@@ -32,7 +33,7 @@ function Opinions() {
             </div>
             <div className='opinions'>
                 <span className='client-one'>
-                    <p className='client-one-opinion'>"Nunca experimentei nada igual! Uma delícia!"</p>
+                    <p className='client-one-opinion'>{`"${opnions[1]}"`}</p>
                     <p className='client-one-name'>- {names[1]}</p>
                 </span>
                 <span className='client-one'>
