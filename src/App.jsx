626512abe2d2c194c0f4802ee31dbbf99efdac04
OxeAlex23 <souzaalexciano@gmail.com>
2025-10-05 11:30:08 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header";
+import Header from "./components/Header";
 import Principal from "./components/Principal";
 import Footer from "./components/Footer";
 import Opinions from "./components/Opinions";
